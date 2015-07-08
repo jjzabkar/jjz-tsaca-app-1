5cebe579e4ce3348f2a17a8a37e6f0ea037d0def
JJ Zabkar <jjzabkar@gmail.com>
2015-07-08 08:52:02 -0700

    Update OneBusAwayApiService property resolution, yml

--- src/main/java/com/jjz/tsaca/service/OneBusAwayApiService.java
+++ src/main/java/com/jjz/tsaca/service/OneBusAwayApiService.java
@@ -1,26 +1,27 @@
 package com.jjz.tsaca.service;
 
-import javax.annotation.PostConstruct;
-
-import org.apache.commons.lang.ObjectUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.bind.RelaxedPropertyResolver;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
-public class OneBusAwayApiService {
+public class OneBusAwayApiService implements EnvironmentAware {
 
-	public static final String OBA_API_KEY = "OBA_API_KEY";
+	public static final String OBA_PROPERTY_PREFIX = "onebusaway.";
+	public static final String OBA_API_KEY = "apiKey";
 
 	private final Logger log = LoggerFactory.getLogger(OneBusAwayApiService.class);
 
 	private String apiKey;
 
-	@PostConstruct
-	public void postConstruct() {
-		apiKey = (String) ObjectUtils.defaultIfNull(System.getenv(OBA_API_KEY), "TEST");
+	@Override
+	public void setEnvironment(Environment env) {
+		RelaxedPropertyResolver propertyResolver = new RelaxedPropertyResolver(env, OBA_PROPERTY_PREFIX);
+		this.apiKey = propertyResolver.getProperty(OBA_API_KEY);
+		log.info("Created OneBusAwayApiService({})", this.apiKey);
 	}
 
 	public final String getApiKey() {
