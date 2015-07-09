ca88ff008e10ec0b6b1468f392b3bc3aed6250af
JJ Zabkar <jjzabkar@gmail.com>
2015-07-09 15:06:33 -0700

    Add OneBusAwayApiStopService

--- src/main/java/com/jjz/tsaca/web/rest/StationResource.java
+++ src/main/java/com/jjz/tsaca/web/rest/StationResource.java
@@ -1,20 +1,27 @@
 package com.jjz.tsaca.web.rest;
 
-import com.codahale.metrics.annotation.Timed;
-import com.jjz.tsaca.domain.Station;
-import com.jjz.tsaca.repository.StationRepository;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Optional;
+
+import javax.inject.Inject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.inject.Inject;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.List;
-import java.util.Optional;
+import com.codahale.metrics.annotation.Timed;
+import com.jjz.tsaca.domain.Station;
+import com.jjz.tsaca.repository.StationRepository;
+import com.jjz.tsaca.service.OneBusAwayApiStopService;
 
 /**
  * REST controller for managing Station.
@@ -28,6 +35,9 @@ public class StationResource {
     @Inject
     private StationRepository stationRepository;
 
+	@Inject
+	private OneBusAwayApiStopService obaStopService;
+
     /**
      * POST  /stations -> Create a new station.
      */
@@ -40,7 +50,7 @@ public class StationResource {
         if (station.getId() != null) {
             return ResponseEntity.badRequest().header("Failure", "A new station cannot already have an ID").body(null);
         }
-        Station result = stationRepository.save(station);
+		Station result = obaStopService.save(station);
         return ResponseEntity.created(new URI("/api/stations/" + station.getId())).body(result);
     }
 
@@ -56,7 +66,7 @@ public class StationResource {
         if (station.getId() == null) {
             return create(station);
         }
-        Station result = stationRepository.save(station);
+		Station result = obaStopService.save(station);
         return ResponseEntity.ok().body(result);
     }
 
