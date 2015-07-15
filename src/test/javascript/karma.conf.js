0d4891723aaf8bc384ddc47c946c823d6d6cd7da
JJ Zabkar <jjzabkar@gmail.com>
2015-07-15 15:28:25 -0700

    Fix npm build

--- src/test/javascript/karma.conf.js
+++ src/test/javascript/karma.conf.js
@@ -13,8 +13,6 @@ module.exports = function (config) {
         files: [
             // bower:js
             'main/webapp/bower_components/modernizr/modernizr.js',
-            'main/webapp/bower_components/jquery/dist/jquery.js',
-            'main/webapp/bower_components/bootstrap/dist/js/bootstrap.js',
             'main/webapp/bower_components/json3/lib/json3.js',
             'main/webapp/bower_components/angular/angular.js',
             'main/webapp/bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
