4738a9c9a8bb1b04fb5f9e088dc54319272d428d
JJ Zabkar <jjzabkar@gmail.com>
2015-07-15 14:14:50 -0700

    Consume /api/arrivals JSON endpoint (UI)
    Verification:
    
    $ curl http://localhost:8080/api/arrivals
    
    {
      "stations": [
        {
          "id": 1,
          "name": "3rd Ave & Pike St",
          "outputSlots": 8,
          "routes": null,
          "stopId": "1_431",
          "travelTimeFromHomeToStationInSeconds": 420,
          "arrivals": [
            {
              "routeId": "1_100101",
              "tripId": "1_28321617",
              "serviceDate": 1436943600000,
              "stopId": "1_431",
              "stopSequence": 3,
              "routeShortName": "21",
              "routeLongName": "",
              "tripHeadsign": "WESTWOOD VILLAGE VIA 35TH AVE SW",
              "scheduledArrivalTime": 1436993395000,
              "scheduledDepartureTime": 1436993395000,
              "distanceFromStop": -751.4076294986153,
              "color": "off",
              "myEstimatedBoardableTime": -933
            },
    ...

--- src/main/webapp/scripts/app/entities/arrival/arrival.controller.js
+++ src/main/webapp/scripts/app/entities/arrival/arrival.controller.js
@@ -1,37 +1,30 @@
 'use strict';
 
-angular.module('jjztsacaapp1App')
-    .controller('ArrivalController', function ($scope, Arrival) {
-        $scope.arrivals = [];
-        $scope.loadAll = function() {
-            Arrival.query(function(result) {
-               $scope.arrivals = result;
-            });
-        };
-        $scope.loadAll();
+angular.module('jjztsacaapp1App').controller('ArrivalController', function($scope, Arrival) {
+	$scope.arrivals = {};
+	$scope.maxOutputSlots = 2;
+	$scope.loadAll = function loadAllFn() {
+		Arrival.query(function ArrivalQueryFn(result) {
+			$scope.arrivals = result;
+			if (result && result.stations) {
+				angular.forEach(result.stations, function forStationFn(station) {
+					$scope.maxOutputSlots = Math.max(station.outputSlots, $scope.maxOutputSlots);
+				});
+			}
+		});
+	};
+	$scope.loadAll();
 
-        $scope.delete = function (id) {
-            Arrival.get({id: id}, function(result) {
-                $scope.arrival = result;
-                $('#deleteArrivalConfirmation').modal('show');
-            });
-        };
+	$scope.refresh = function() {
+		$scope.loadAll();
+		$scope.clear();
+	};
 
-        $scope.confirmDelete = function (id) {
-            Arrival.delete({id: id},
-                function () {
-                    $scope.loadAll();
-                    $('#deleteArrivalConfirmation').modal('hide');
-                    $scope.clear();
-                });
-        };
-
-        $scope.refresh = function () {
-            $scope.loadAll();
-            $scope.clear();
-        };
-
-        $scope.clear = function () {
-            $scope.arrival = {foo: null, bar: null, id: null};
-        };
-    });
+	$scope.clear = function() {
+		$scope.arrival = {
+			foo : null,
+			bar : null,
+			id : null
+		};
+	};
+});
