221b13ff5675369b2cce6c728b450dcb74a721d6
JJ Zabkar <jjzabkar@gmail.com>
2015-07-15 14:06:46 -0700

    Format log msg

--- src/main/java/com/jjz/tsaca/aop/logging/LoggingAspect.java
+++ src/main/java/com/jjz/tsaca/aop/logging/LoggingAspect.java
@@ -48,7 +48,7 @@ public class LoggingAspect {
         try {
             Object result = joinPoint.proceed();
             if (log.isDebugEnabled()) {
-                log.debug("Exit: {}.{}() with result = {}", joinPoint.getSignature().getDeclaringTypeName(),
+                log.debug("Exit:  {}.{}() with result = {}", joinPoint.getSignature().getDeclaringTypeName(),
                         joinPoint.getSignature().getName(), result);
             }
             return result;
